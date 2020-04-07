2a4cd69f6642667e6142777c1291bfe94389e8b6
williamerwin <39997584+williamerwin@users.noreply.github.com>
2020-04-07 12:03:17 -0700

    Create skumap.js

---
+++ skumap.js
@@ -0,0 +1,104 @@
+var sku_lookup_map = {};
+sku_lookup_map 
+
+ //Rennie 3 Month Buyouts Delivery 
+     ['Rennie' + 'Weekly' + '3months' + 'delivery'] = {sku: 'sku_H39Rsh9Vk7kLav', quantity: 1};
+     ['Rennie' + 'Bi-Weekly' + '3months' + 'delivery'] = {sku: 'sku_H3B5ShvsGQxQse', quantity: 1};
+     ['Rennie' + 'Monthly' + '3months' + 'delivery'] = {sku: 'sku_H3B5I56VXy9utQ', quantity: 1};
+ //Rennie 3 Month Buyouts Pickup
+     ['Rennie' + 'Weekly' + '3months' + 'pickup'] = {sku: 'sku_H398HhZJqtw39j', quantity: 1};
+     ['Rennie' + 'Bi-Weekly' + '3months' + 'pickup'] = {sku: 'sku_H39LoQUyXFQWoR', quantity: 1};
+     ['Rennie' + 'Monthly' + '3months' + 'pickup'] = {sku: 'sku_H39In8RoA5Pkb9', quantity: 1};
+ //Rennie 6 Month Buyouts Delivery
+     ['Rennie' + 'Weekly' + '6months' + 'delivery'] = {sku: 'sku_H3AcTIESxwLHew', quantity: 1};
+     ['Rennie' + 'Bi-Weekly' + '6months' + 'delivery'] = {sku: 'sku_H3B625rOFsFKg6', quantity: 1};
+     ['Rennie' + 'Monthly' + '6months' + 'delivery'] = {sku: 'sku_H3B6S9ssSNdUIM', quantity: 1};
+  //Rennie 6 Month Buyouts Pickup
+     ['Rennie' + 'Weekly' + '6months' + 'pickup'] = {sku: 'sku_H3BMuaq8CIhtxh', quantity: 1};
+     ['Rennie' + 'Bi-Weekly' + '6months' + 'pickup'] = {sku: 'sku_H3BMX3r1F25eMy', quantity: 1};
+     ['Rennie' + 'Monthly' + '6months' + 'pickup'] = {sku: 'sku_H3AcpkLnWAWefp', quantity: 1};
+  //Rennie 12 Month Buyouts Delivery
+     ['Rennie' + 'Weekly' + '12months' + 'delivery'] = {sku: 'sku_H3BP1Qul3LGDvd', quantity: 1};
+     ['Rennie' + 'Bi-Weekly' + '12months' + 'delivery'] = {sku: 'sku_H3BPocqOxSgOCd', quantity: 1};
+     ['Rennie' + 'Monthly' + '12months' + 'delivery'] = {sku: 'sku_H3AdaZOvdcBGvZ', quantity: 1};
+  //Rennie 12 Month Buyouts Pickup
+     ['Rennie' + 'Weekly' + '12months' + 'pickup'] = {sku: 'sku_H3BQTLQHi4vgWP', quantity: 1};
+     ['Rennie' + 'Bi-Weekly' + '12months' + 'pickup'] = {sku: 'sku_H3BQ67FjSLMxDB', quantity: 1};
+     ['Rennie' + 'Monthly' + '12months' + 'pickup'] = {sku: 'sku_H3Ad8efbUL59to', quantity: 1};
+ 
+ 
+  //Vernon 3 Month Buyouts Delivery 
+     ['Vernon' + 'Weekly' + '3months' + 'delivery'] = {sku: 'sku_H3Be9TAjm2oxhI', quantity: 1};
+     ['Vernon' + 'Bi-Weekly' + '3months' + 'delivery'] = {sku: 'sku_H3AmXKeW1TdZy1', quantity: 1};
+     ['Vernon' + 'Monthly' + '3months' + 'delivery'] = {sku: 'sku_H3Bfv49rr76NJg', quantity: 1};
+ //Vernon 3 Month Buyouts Pickup
+     ['Vernon' + 'Weekly' + '3months' + 'pickup'] = {sku: 'sku_H3BbaSD7EMhTKK', quantity: 1};
+     ['Vernon' + 'Bi-Weekly' + '3months' + 'pickup'] = {sku: 'sku_H3AmwA49vESa1B', quantity: 1};
+     ['Vernon' + 'Monthly' + '3months' + 'pickup'] = {sku: 'sku_H3Bbacy0TEZ5Bu', quantity: 1};
+ //Vernon 6 Month Buyouts Delivery
+     ['Vernon' + 'Weekly' + '6months' + 'delivery'] = {sku: 'sku_H3BfdkFtukrfaf', quantity: 1};
+     ['Vernon' + 'Bi-Weekly' + '6months' + 'delivery'] = {sku: 'sku_H3An7ARlPIgepS', quantity: 1};
+     ['Vernon' + 'Monthly' + '6months' + 'delivery'] = {sku: 'sku_H3BfKF9OqIFNsn', quantity: 1};
+  //Vernon 6 Month Buyouts Pickup
+     ['Vernon' + 'Weekly' + '6months' + 'pickup'] = {sku: 'sku_H3BcW0oDzHLM99', quantity: 1};
+     ['Vernon' + 'Bi-Weekly' + '6months' + 'pickup'] = {sku: 'sku_H3AnvfZxDeKzPZ', quantity: 1};
+     ['Vernon' + 'Monthly' + '6months' + 'pickup'] = {sku: 'sku_H3BcWOVmM9h7cn', quantity: 1};
+  //Vernon 12 Month Buyouts Delivery
+     ['Vernon' + 'Weekly' + '12months' + 'delivery'] = {sku: 'sku_H3BgFpqTEIFEaC', quantity: 1};
+     ['Vernon' + 'Bi-Weekly' + '12months' + 'delivery'] = {sku: 'sku_H3AovWN1knV9KM', quantity: 1};
+     ['Vernon' + 'Monthly' + '12months' + 'delivery'] = {sku: 'sku_H3BgluDjN7WNLr', quantity: 1};
+  //Vernon 12 Month Buyouts Pickup
+     ['Vernon' + 'Weekly' + '12months' + 'pickup'] = {sku: 'sku_H3BdXuaFVkhsTZ', quantity: 1};
+     ['Vernon' + 'Bi-Weekly' + '12months' + 'pickup'] = {sku: 'sku_H3AngIl9AxVPRp', quantity: 1};
+     ['Vernon' + 'Monthly' + '12months' + 'pickup'] = {sku: 'sku_H3BeqkM3CwqyhK', quantity: 1};
+
+    //Clement 3 Month Buyouts Delivery 
+     ['Clement' + 'Weekly' + '3months' + 'delivery'] = {sku: 'sku_H3BiffDgUdwYSd', quantity: 1};
+     ['Clement' + 'Bi-Weekly' + '3months' + 'delivery'] = {sku: 'sku_H3B1foxqFREbFG', quantity: 1};
+     ['Clement' + 'Monthly' + '3months' + 'delivery'] = {sku: 'sku_H3BiPWI0G8lFYM', quantity: 1};
+ //Clement 3 Month Buyouts Pickup
+     ['Clement' + 'Weekly' + '3months' + 'pickup'] = {sku: 'sku_H3B12p31RnqZhO', quantity: 1};
+     ['Clement' + 'Bi-Weekly' + '3months' + 'pickup'] = {sku: 'sku_H3BjnBCQCEHXMU', quantity: 1};
+     ['Clement' + 'Monthly' + '3months' + 'pickup'] = {sku: 'sku_H3Bjqv5WJc0BAP', quantity: 1};
+ //Clement 6 Month Buyouts Delivery
+     ['Clement' + 'Weekly' + '6months' + 'delivery'] = {sku: 'sku_H3BhESrSxb1n26', quantity: 1};
+     ['Clement' + 'Bi-Weekly' + '6months' + 'delivery'] = {sku: 'sku_H3B2TGTvAOEJ1v', quantity: 1};
+     ['Clement' + 'Monthly' + '6months' + 'delivery'] = {sku: 'sku_H3BiMRYJw9BEc9', quantity: 1};
+  //Clement 6 Month Buyouts Pickup
+     ['Clement' + 'Weekly' + '6months' + 'pickup'] = {sku: 'sku_H3Bk1NGveoihfk', quantity: 1};
+     ['Clement' + 'Bi-Weekly' + '6months' + 'pickup'] = {sku: 'sku_H3BkKIddkuLhih', quantity: 1};
+     ['Clement' + 'Monthly' + '6months' + 'pickup'] = {sku: 'sku_H3B2Jc5UrwcM4t', quantity: 1};
+  //Clement 12 Month Buyouts Delivery
+     ['Clement' + 'Weekly' + '12months' + 'delivery'] = {sku: 'sku_H3Bhvej8oOhPEK', quantity: 1};
+     ['Clement' + 'Bi-Weekly' + '12months' + 'delivery'] = {sku: 'sku_H3B38yfdSuiPxI', quantity: 1};
+     ['Clement' + 'Monthly' + '12months' + 'delivery'] = {sku: 'sku_H3Bh4oVPgDyntZ', quantity: 1};
+  //Clement 12 Month Buyouts Pickup
+     ['Clement' + 'Weekly' + '12months' + 'pickup'] = {sku: 'sku_H3B2TlVderzf03', quantity: 1};
+     ['Clement' + 'Bi-Weekly' + '12months' + 'pickup'] = {sku: 'sku_H3Bk6C5IPqlzsY', quantity: 1};
+     ['Clement' + 'Monthly' + '12months' + 'pickup'] = {sku: 'sku_H3BlmIboH8nnlY', quantity: 1};
+
+
+  //Ongoing Subscriptions Rennie
+      ['Rennie' + 'Weekly' + 'Ongoing' + 'pickup'] = {plan: 'plan_H3951gFFbNceDy', quantity: 1};
+      ['Rennie' + 'Bi-Weekly' + 'Ongoing' + 'pickup'] = {plan: 'plan_H3967B7TzSpsUC', quantity: 1};
+      ['Rennie' + 'Monthly' + 'Ongoing' + 'pickup'] = {plan: 'plan_H396U3mPTZ6LWb', quantity: 1};
+      ['Rennie' + 'Weekly' + 'Ongoing' + 'delivery'] = {plan: 'plan_H391NmRnCEC27Z', quantity: 1};
+      ['Rennie' + 'Bi-Weekly' + 'Ongoing' + 'delivery'] = {plan: 'plan_H391cBDIDiHk5Y', quantity: 1};
+      ['Rennie' + 'Monthly' + 'Ongoing' + 'delivery'] = {plan: 'plan_H392SbTbMdQxeu', quantity: 1};
+
+ //Ongoing Subscriptions Vernon
+     ['Vernon' + 'Weekly' + 'Ongoing' + 'pickup'] = {plan: 'plan_H3HNleXY1w0hnu', quantity: 1};
+     ['Vernon' + 'Bi-Weekly' + 'Ongoing' + 'pickup'] = {plan: 'plan_H3HNRSdsivpYD0', quantity: 1};
+     ['Vernon' + 'Monthly' + 'Ongoing' + 'pickup'] = {plan: 'plan_H3HNleXY1w0hnu', quantity: 1};
+     ['Vernon' + 'Weekly' + 'Ongoing' + 'delivery'] = {plan: 'plan_H3HPIDH7hqLG9M', quantity: 1};
+     ['Vernon' + 'Bi-Weekly' + 'Ongoing' + 'delivery'] = {plan: 'plan_H3HPbEGgH8lP8W', quantity: 1};
+     ['Vernon' + 'Monthly' + 'Ongoing' + 'delivery'] = {plan: 'plan_H3HPgkZrNeh88B', quantity: 1};
+
+ //Ongoing Subscriptions Clement
+     ['Clement' + 'Weekly' + 'Ongoing' + 'pickup'] = {plan: 'plan_H3HIDbgpne2clT', quantity: 1};
+     ['Clement' + 'Bi-Weekly' + 'Ongoing' + 'pickup'] = {plan: 'plan_H3HHC6O1sCiMYj', quantity: 1};
+     ['Clement' + 'Monthly' + 'Ongoing' + 'pickup'] = {plan: 'plan_H3HIzzeOrxQQGD', quantity: 1};
+     ['Clement' + 'Weekly' + 'Ongoing' + 'delivery'] = {plan: 'plan_H3HLoVzBETkflX', quantity: 1};
+     ['Clement' + 'Bi-Weekly' + 'Ongoing' + 'delivery'] = {plan: 'plan_H3HKLWofGg7KVJ', quantity: 1};
+     ['Clement' + 'Monthly' + 'Ongoing' + 'delivery'] = {plan: 'plan_H3HLEzWHiKYfGp', quantity: 1};
+ 
